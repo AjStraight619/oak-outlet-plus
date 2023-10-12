1b40028344bf2edb386cc7531a69ba65e67633ac
Alex Straight <compsciaj@outlook.com>
2023-10-11 19:36:17 -0700

    ..

--- components/PhotoDisplay.tsx
+++ components/PhotoDisplay.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 const PhotoDisplay = ({ photos }: any) => {
   return (
     <div>
       {photos && photos.length > 0 ? (
         photos.map((photo: any, index: number) => (
-          <img key={index} src={photo.url} alt={`Photo ${index + 1}`} />
+          <Image key={index} src={photo.url} alt={`Photo ${index + 1}`} />
         ))
       ) : (
         <p>No photos available</p>
